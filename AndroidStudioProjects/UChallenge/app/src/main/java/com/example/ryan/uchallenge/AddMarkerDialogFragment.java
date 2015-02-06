9db2693f9e4bfdb7f9e9e241d8c846e23b49a0b6
Montoyaman <rymo8067@colorado.edu>
2015-02-06 16:30:27 -0700

    Initial commit

---
+++ AndroidStudioProjects/UChallenge/app/src/main/java/com/example/ryan/uchallenge/AddMarkerDialogFragment.java
@@ -0,0 +1,31 @@
+package com.example.ryan.uchallenge;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by ryan on 2/5/15.
+ */
+public class AddMarkerDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_add_title)
+                .setPositiveButton(R.string.enter, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
