484cbc6bbc4c94db3751d92ea68fb83196e610e2
Montoyaman <rymo8067@colorado.edu>
2015-02-08 00:04:30 -0700

    Added dialog and marker infowindows

--- AndroidStudioProjects/UChallenge/app/src/main/java/com/example/ryan/uchallenge/MainGUI.java
+++ AndroidStudioProjects/UChallenge/app/src/main/java/com/example/ryan/uchallenge/MainGUI.java
@@ -1,49 +1,158 @@
 package com.example.ryan.uchallenge;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-//import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainGUI extends FragmentActivity {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleMap mMap;
+
+    LatLng curPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*Setup the theme and map*/
         setTheme(R.style.SplashTheme);
-
         setContentView(R.layout.activity_main_gui);
         setUpMapIfNeeded();
 
-        final AddMarkerDialogFragment dialogAdd = new AddMarkerDialogFragment();
+        // Setting a custom info window adapter for the map
+//        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+//
+//            // Use default InfoWindow frame
+//            @Override
+//            public View getInfoWindow(Marker arg0) {
+//                return null;
+//            }
+//
+//            // Defines the contents of the InfoWindow
+//            @Override
+//            public View getInfoContents(Marker arg0) {
+//
+//                // Getting view from the layout file info_window_layout
+//                View v = getLayoutInflater().inflate(R.layout.windowlayout, null);
+//
+//                // Getting the position from the marker
+//                LatLng latLng = arg0.getPosition();
+//
+//                // Getting reference to the TextView to set latitude
+//                //TextView Title = (TextView) v.findViewById(R.id.Title);
+//
+//                // Getting reference to the TextView to set longitude
+//                //TextView Description = (TextView) v.findViewById(R.id.Description_);
+//
+//                // Setting the latitude
+//                //tvLat.setText("Latitude:" + latLng.latitude);
+//
+//                // Setting the longitude
+//                //tvLng.setText("Longitude:"+ latLng.longitude);
+//
+//                // Returning the view containing InfoWindow contents
+//                return v;
+//
+//            }
+//        });
 
+        //Check when the info window is clicked
+//        mMap.setOnInfoWindowClickListener(
+//                new GoogleMap.OnInfoWindowClickListener() {
+//                    @Override
+//                    public void onInfoWindowClick(Marker marker) {
+//                        marker.setTitle("Blah");
+//                        marker.showInfoWindow();
+//                    }
+//                }
+//        );
+
+        //Add a dialog box
+        final Dialog dialog = new Dialog(MainGUI.this);
+        dialog.setContentView(R.layout.marker_add_dialog);
+
+
+
+        //final AddMarkerDialogFragment dialogAdd = new AddMarkerDialogFragment();
+
+        //Setup the map click listener
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
             @Override
             public void onMapClick(LatLng point) {
-                MarkerOptions options = new MarkerOptions();
-
                 /*Pass marker parameters into dialog*/
-                Bundle markBundle = new Bundle();
-                markBundle.putDouble("Longitude",point.longitude);
-                markBundle.putDouble("Latitude",point.latitude);
+                //Bundle markBundle = new Bundle();
+                //markBundle.putDouble("Longitude",point.longitude);
+                //markBundle.putDouble("Latitude",point.latitude);
 
-                dialogAdd.setArguments(markBundle);
-                dialogAdd.show(getFragmentManager(),"Marker");
-                mMap.addMarker(options.position(point).title("Marker"));
+                curPoint = point;
+                dialog.setTitle("Add a Challenge");
+                dialog.show();
+
+                Button OKButton = (Button) dialog.findViewById(R.id.OK);
+                OKButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MarkerOptions options = new MarkerOptions();
+
+                        EditText title = (EditText)dialog.findViewById(R.id.addTitle);
+                        EditText description = (EditText) dialog.findViewById(R.id.addDescription);
+
+                        options.title(title.getText().toString());
+                        options.snippet(description.getText().toString());
+
+                        // Setting position on the MarkerOptions
+                        options.position(curPoint);
+
+                        // Animating to the currently touched position
+                        mMap.animateCamera(CameraUpdateFactory.newLatLng(curPoint));
+
+                        // Adding marker on the GoogleMap
+                        Marker marker = mMap.addMarker(options);
+
+                        // Showing InfoWindow on the GoogleMap
+                        marker.showInfoWindow();
+
+                        dialog.dismiss();
+
+                        curPoint = null;
+                    }
+                });
+                //dialogAdd.setArguments(markBundle);
+                //Display the dialog!
+                //dialogAdd.set
+                //dialogAdd.show(getFragmentManager(), "Marker");
             }
         });
+    }
+
+    //User has determined to create a marker, so make it!
+    public void onUserSelectValue(String selectedValue, LatLng point)
+    {
+        MarkerOptions options = new MarkerOptions();
+        // Setting position on the MarkerOptions
+        options.position(point);
+
+        // Animating to the currently touched position
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(point));
+
+        // Adding marker on the GoogleMap
+        Marker marker = mMap.addMarker(options);
+
+        // Showing InfoWindow on the GoogleMap
+        marker.showInfoWindow();
     }
 
     @Override
